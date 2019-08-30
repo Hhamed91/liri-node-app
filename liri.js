2b912a65b1af9195dd16cf6a718d812dfa4d1fc9
user.name <hazem.hamed.h@hotmail.com>
2019-08-29 17:36:27 -0700

    Make a JavaScript file named liri.js - add code to read and set any environment variables with the dotenv packag

---
+++ liri.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+var fs = require("fs");
+var keys = require("./keys.js");
+var axios= require("axios");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
