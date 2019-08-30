e2f0211eddcb2d8ff76166af5a6f62dd7c28f1f4
user.name <hazem.hamed.h@hotmail.com>
2019-08-29 17:42:41 -0700

    Add process.argv

--- liri.js
+++ liri.js
@@ -4,3 +4,6 @@ var keys = require("./keys.js");
 var axios= require("axios");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var searchValue = process.argv[3];
