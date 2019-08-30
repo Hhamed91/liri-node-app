e25e37867b5ceb3c70e5229041544967f2813acd
user.name <hazem.hamed.h@hotmail.com>
2019-08-29 18:07:06 -0700

    calling omdbapi url using axios GET call and consloe log reuslts

--- liri.js
+++ liri.js
@@ -8,62 +8,78 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var searchValue = process.argv[3];
 
-function Spotify_This_Song (searchValue){
+function Spotify_This_Song(searchValue) {
 
-    if(searchValue===undefined || null){
-    searchValue="The sign";
+    if (searchValue === undefined || null) {
+        searchValue = "The sign";
     }
-        spotify.search({ type: 'track', query: searchValue }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
+    spotify.search({ type: 'track', query: searchValue }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        else {
+            for (let i = 0; i < data.tracks.items.length && i < 5; i++) {
+
             }
-            
-            else{
-                for (let i = 0; i < data.tracks.items.length && i<5; i++) {
-                
-                    }
             console.log("🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶")
             console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
             console.log("The song's name: " + data.tracks.items[0].name)
             console.log("A preview link of the song from Spotify: " + data.tracks.items[0].href)
             console.log("The album that the song is from: " + data.tracks.items[0].album.name)
             console.log("🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶🎶")
-            }
-        
-        });
-        };
+        }
+
+    });
+};
 
 
-    function movie_This(searchValue) {
-        var queryUrl = "http://www.omdbapi.com/?t=" + searchValue + "&y=&plot=short&apikey=trilogy";
-        console.log(queryUrl);
+function movie_This(searchValue) {
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchValue + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+
+    console.log("✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎")
 
 
-    };
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Title of the Movie : " + response.data.Title);
+            console.log("Year the movie came out : " + response.data.Year);
+            console.log("Rotten Tomatoes Rating of the Movie : " + response.data.Ratings[0].Value);
+            console.log("Country where the  movie was produced : " + response.data.Country);
+            console.log("Language of the Movie : " + response.data.Language);
+            console.log("Plot of the Movie  : " + response.data.Plot);
+            console.log("Actors in the Movie : " + response.data.Actors);
+            console.log("✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎✎")
+        }
+    );
 
-    function concert_This(searchValue) {
 
-    }
+};
 
-    function do_what_it_says() {
+function concert_This(searchValue) {
 
-    }
+}
 
-    switch (command) {
+function do_what_it_says() {
 
-        case "spotify-this-song":
-            Spotify_This_Song(searchValue);
-            break;
+}
 
-        case "movie-this":
-            movie_This(searchValue);
-            break;
+switch (command) {
 
-        case "concert-this":
-            concert_This(searchValue);
-            break;
+    case "spotify-this-song":
+        Spotify_This_Song(searchValue);
+        break;
 
-        case "do-what-it-says":
-            do_what_it_says();
-            break;
-    }
+    case "movie-this":
+        movie_This(searchValue);
+        break;
+
+    case "concert-this":
+        concert_This(searchValue);
+        break;
+
+    case "do-what-it-says":
+        do_what_it_says();
+        break;
+}
